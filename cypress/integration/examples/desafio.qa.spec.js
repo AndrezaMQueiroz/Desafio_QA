72fe4f5aaa05da44f710b5a180f3a138b4f87104
andreza Martins <andreza.m.queiroz@gmail.com>
2020-08-25 23:05:19 -0300

    Challenge part two
    Challenge part two.
    Access the elements inside the iframe and press the square

--- cypress/integration/examples/desafio.qa.spec.js
+++ cypress/integration/examples/desafio.qa.spec.js
@@ -1,22 +1,41 @@
 /// <reference types="cypress" />
 
-describe('Desafio QA', () => {
-    before(() => {
-        cy.visit('https://codepen.io/choskim/pen/RLYebL');
-        
+describe('Desafio_QA', () => {
+    beforeEach(() => {
+        // access the chanllenge URL
+        cy.visit('https://codepen.io/choskim/pen/RLYebL')
+
+        // it gets the iframe instance
+        cy.getIframeBody().as('iframe')
     });
 
-    it('Desafio', () => {
-        cy.get('iframe[id=result]').then(iframe => {
-            const body = iframe.contents().find("body");
-            cy.wrap(body)
-            .click();
-        });
-    });
+    it('should change the square size after long press', () => {
+        // it gets the square element instance
+        cy.get('@iframe')
+            .find('.square')
+            .first().as('square');
 
-    it.only('Desafio 2', () => {
-        cy.getIframeBody()
-        .wrap('.square') .click();
+        // First press: it presses the square for 500 ms and then releases it
+        cy.get('@square')
+            .trigger("pointerdown", { button: 0 })
+            .wait(500)
+            .get('@square')
+            .trigger('pointerleave', { button: 0 });
 
+        // It validates the square size after clicked for the first time
+        cy.get('@square')
+            .should('have.css', 'height', '225px')
+            .should('have.css', 'width', '225px');
+
+        // Second press: it presses the square for 500 ms and then releases it
+        cy.get('@square')
+            .trigger("pointerdown", { button: 0 })
+            .wait(500)
+            .get('@square')
+            .trigger('pointerleave', { button: 0 });
+
+        // It validates the square size after clicked for the second time
+        cy.get('@square').should('have.css', 'height', '90px')
+            .get('@square').should('have.css', 'width', '90px');
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,14 +23,17 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
 Cypress.Commands.add('getIframeBody', () => {
     // get the iframe > document > body
     // and retry until the body element is not empty
+    cy.log('getIframeBody');
+
     return cy
-    .get('iframe[id=result]')
-    .should('not.be.empty')
-    // wraps "body" DOM element to allow
-    // chaining more Cypress commands, like ".find(...)"
-    // https://on.cypress.io/wrap
-    .then(cy.wrap)
-  });
+        .get('iframe[id="result"]', { log: false })
+        .its('0.contentDocument.body', { log: false }).should('not.be.empty')
+        // wraps "body" DOM element to allow
+        // chaining more Cypress commands, like ".find(...)"
+        // https://on.cypress.io/wrap
+        .then((body) => cy.wrap(body, { log: false }));
+})
